7b3a5ffa3b1c22d2cace88e8cba7b34df2c2fbf7
Jack Torn <j4cktorn@gmail.com>
2022-10-03 20:52:19 +0100

    301 day 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import Cardz from './cards';
 
-function App() {
+
+
+const App = () => {
+
+const Customer = [
+  {
+    customerName:"Jack Torn",
+    address: "8 Davidson Road",
+    postcode: "NR7 0XW",
+    phone: 123456789,
+    chimneyType: "w/b stove",
+    brushType: "6 inch",
+    guard: "yes",
+    },
+    
+    {
+      customerName:"Mark Ball",
+      address: "A Street",
+      postcode: "NR4 PO5H",
+      phone: 123456789,
+      chimneyType: "m/f stove",
+      brushType: "6 inch",
+      guard: "yes",
+      },
+  
+];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <div className="header">
+      <h1 className="main-text"> First React Page - 29th Sept 2022 </h1>
+      </div>
+
+      
+
+      <div className="card-container">
+        
+        {/* step 1 - movies.map() */}
+        {Customer.map((value) => {
+          return(
+            <Cardz data = {value} />
+
+            // <div className="card">
+            //   <h1>Customer Card</h1>
+            //   <h1>{value.customerName}</h1>
+            //   <h3>{value.address}</h3>
+            //   <h3>{value.postcode}</h3>
+            //   <h3>{value.phone}</h3>
+            //   <h3>{value.chimneyType}</h3>
+            //   <h3>{value.brushType}</h3>
+            //   <h3>{value.guard}</h3>
+            // </div>
+          );
+        })}
+        {/*{ step 2 - movies.map( () => {} ) }*/}
+
+        {/* step 3 - movies.map( () => {
+          return();
+        } ) */}
+
+        {/* step 4 - movies.map( () => {
+          return(
+            <div>
+            // Do the stuff here
+            </div>
+          );
+        } ) */}
+
+        {/* step 5 - movies.map( (value) => {
+          return(
+            <div>
+            // Do the stuff here
+            <p>{value}</p> 
+            </div>
+          );
+        } ) */}
+        
+
+      </div>
+      <div className="footer"></div>
+
+
+
+
     </div>
+  
+    
   );
 }
 
---
+++ src/cards.js
@@ -0,0 +1,37 @@
+import Customer from './App.css';
+
+const Cardz = (props) => {
+
+    const customerName = props.data.customerName;
+    const address = props.data.address;
+    const postcode = props.data.postcode;
+    const phone = props.data.phone;
+    const chimneyType = props.data.chimneyType;
+    const brushType = props.data.brushType;
+    const guard = props.data.guard;
+    
+    
+  
+  
+    return(
+        <div>
+            <h1>{customerName}</h1>
+            <h3>{address}</h3>
+               <h3>{postcode}</h3>
+               <h3>{phone}</h3>
+               <h3>{chimneyType}</h3>
+               <h3>{brushType}</h3>
+              <h3>{guard}</h3>
+        </div>
+    );
+
+
+};
+
+
+export default Cardz;
+
+
+
+
+<Customer/>
